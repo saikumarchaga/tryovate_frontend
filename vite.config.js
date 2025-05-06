22b13628f62b2822118b7ac18521b34fe077cc68
saikumarchaga <99958553+saikumarchaga@users.noreply.github.com>
2025-05-06 16:58:29 +0530

    Update vite.config.js
    change the server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+//import react from "@vitejs/plugin-react";
+//import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+//export default defineConfig({
+  //plugins: [react()],
+//});
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,        // 👈 expose to LAN / public IP
+    port: 5173,        // 👈 you can change this if needed
+  },
 });
